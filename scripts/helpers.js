5b8c4c149bd093cb6b979158ad59cdbaca512081
Adam Olson <adam.lee.olson@outlook.com>
2023-08-05 09:53:29 -0500

    mostly finished, need to pass in some elements yet

--- scripts/helpers.js
+++ scripts/helpers.js
@@ -1,25 +1,84 @@
 const buildFeaturesList = features => features
-.map(string => `<li>${string}</li>`)
-.join('')
- 
+    .map(string => `<li>${string}</li>`)
+    .join('')
 
+function renderLicenseLink(license) {
+    return `[License Info](https://choosealicense.com/licenses/)`
+}
 
+function renderLicenseBadge(license) {
+    return `![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+}
 
-export const getGitHubReadmemd = ({name,price, condition,features, weight, numCodeViolations, image}) => {
+function renderLicenseSection(license) {
+    return `${renderLicenseBadge(license)}
+
+    #### License Details
+    ${renderLicenseLink(license)}`
+}
+
+export const getGitHubReadmemd = ({ 
+    name, projectName, license, description, 
+    installation, testing, usage, GithubUserName, 
+    Contributing, email, AuthorName, Roadmap, features }) => {
     return `
-    # Robot Name
+    # Project Name ${projectName}
 
-## Price: $1000
-## Condition: New
-## Weight: 10 kg
-## Num Code Violations: 0
+    [![License](https://img.shields.io/badge/License-MIT-blue.svg)](https://opensource.org/licenses/${license})
+    
+    ## Description
+    
+    A brief description of what the project does and its main features.
+    ${description}
+    ## Table of Contents
+    
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Features](#features)
+    - [Contributing](#contributing)
+    - [License](#license)
+    - [Contact](#contact)
+    
+    ## Installation
+    
+    Step-by-step instructions on how to install and set up the project.
+    ${installation} 
+    
+    ## Usage
+    
+    Instructions on how to use the project, including examples and screenshots.
+    ${usage} 
+    ## Features
+    
+    Highlight the key features and functionalities of the project.
+    
+    ## Contributing
+  ${Contributing}
+    
+    ## License
+    
+    This project is licensed under the ${license}
+    ${renderLicenseSection(license)}
+    
+    ## Contact
+    
+    - Author: [${AuthorName}](https://github.com/${GithubUserName})
+    - Email: ${email} 
+    
+    
+    ## Testing Requirements
+   
+    Project testing needs: ${testing}
+    
 
-## Features
-- Feature 1
-- Feature 2
-- Feature 3
+    
+    ## Roadmap 
+    
+    ${Roadmap}
+    
+    
     
     `// you need to put inside the backticks the md file contentsyet and also, swap out the type names with the correct title names of the md object file. 
-    
+
     //he creates a template literal from the file using a function.  
-    }
+}
--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 import fs from 'fs'
 import inquirer from "inquirer"
-import {getGitHubReadmemd} from './helpers.js'
+import { getGitHubReadmemd } from './helpers.js'
 
 
 
@@ -11,80 +11,123 @@ inquirer.prompt([
 
 
 
-{
+    {
 
-    type: 'input', 
-    name: 'name', 
-    message: 'What is the robot\'s name?', 
-},   //name 
+        type: 'input',
+        name: 'name',
+        message: 'What do you want to call the file?',
+    },
 
-{
+    {
 
-    type: 'number', 
-    name: 'price', 
-    message: 'What is the robot\'s price?', 
-    
-}, // price
+        type: 'input',
+        name: 'projectName',
+        message: 'What is the project name?',
 
-{
+    },
 
-    type: 'list', 
-    name: 'condition', 
-    message: 'What is the robot\'s condition?', 
-    choices: ['new', 'used'],
-}, // condition new or used
+    {
 
-
-{
-
-    type: 'number', 
-    name: 'weight', 
-    message: 'What is the robot\'s weight?', 
-   
-}, //weight 
+        type: 'list',
+        name: 'license',
+        message: 'What License(s) are used?',
+        choices: ["MIT", "ISC", "APACHE 2.0", "GPL", "None"],
+    },
 
 
-{
-    type: 'checkbox', 
-    name: 'features', 
-    message: 'which features are included?',
-    choices: ['can fly', 'can talk', 'can clean']
-    
+    {
 
-},//features
- 
+        type: 'input',
+        name: 'description',
+        message: 'Enter project description: ',
 
-{
-
-    type: 'number', 
-    name: 'numCodeViolations', 
-    message: 'How many code violations have been reported for this robot?', 
-    default: 0,
-   
-}, // code violations
+    },
 
 
+    {
+        type: 'checkbox',
+        name: 'installation',
+        message: 'How is this project installed?',
+        choices: ['git clone', 'npm  i', 'npm start']
 
-{
-    type: 'list', 
-    name: 'image', 
-    message: 'choose an image', 
-    choices: () => {
 
-   const files = fs.readdirSync('./assets/images/thumbs')
-   return files 
+    },
+
+
+    {
+
+        type: 'input',
+        name: 'testing',
+        message: 'How many code violations have been reported for this robot?',
+        default: " Manual testing",
+
+    },
+
+
+
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Enter project usage restrictions: ',
+        default: "Open Source Project"
+    },
+
+    {
+        type: 'input',
+        name: 'GithubUserName',
+        message: 'What is the GitHub Username? ',
+
+    },
+
+
+    {
+        type: 'input',
+        name: 'Contributing',
+        message: 'What contributions this project can accept? ',
+        default: "This project currently does not accept any contribution"
+    },
+
+
+
+
+    {
+        type: 'input',
+        name: 'email',
+        message: 'What is the email address used?  ',
+
+    },
+
+
+
+    {
+        type: 'input',
+        name: 'AuthorName',
+        message: 'What is the name of the author for this project? ',
+
+    }, 
+
+    {
+        type: 'checkbox',
+        name: 'Roadmap',
+        message: 'What future developments or implementations will there be for this project? ',
+        choices: ['Generate HTML files','Generate MArkdown with Images', 'asdfdsfdsd', 'xxxxxxx']
+        
+
     }
-}// image
+
+
+
+    
 
 ])
-.then(answers => {
-    const md= getGitHubReadmemd(answers)
-   fs.writeFile(`./newFiles/${answers.name}.md`, md, err=>{
-    if(err) {
-        throw err
-    }
+    .then(answers => {
+        const md = getGitHubReadmemd(answers)
+        fs.writeFile(`./newFiles/${answers.name}.md`, md, err => {
+            if (err) {
+                throw err
+            }
 
-    console.log(`${answers.name}.md saved!`)
-   })
+            console.log(`${answers.name}.md saved!`)
+        })
     })
-.catch (error => console.log(error))
+    .catch(error => console.log(error))
